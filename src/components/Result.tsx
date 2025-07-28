e59a4a8bfc6cf128e9bcc37ef456f27bbcddb7d4
Lindsay Hartfiel <lkhartfiel@uwalumni.com>
2025-07-27 21:36:37 -0600

    Update styles

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,11 +1,23 @@
 import parsePhoneNumber, { parse } from "libphonenumber-js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGlobe,
+  faLocationDot,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 import { Ratings } from "./Ratings";
 import { InfoBox } from "./Infobox";
 
 const webIcon = (
   <FontAwesomeIcon icon={faGlobe} className="text-h2 text-bright-salmon" />
+);
+
+const phoneIcon = (
+  <FontAwesomeIcon icon={faPhone} className="text-body-sm text-salmon" />
+);
+
+const locationIcon = (
+  <FontAwesomeIcon icon={faLocationDot} className="text-body-sm text-salmon" />
 );
 
 const Result = ({ result, index }) => {
@@ -20,19 +32,25 @@ const Result = ({ result, index }) => {
       {...wrapperProps}
       target="_blank"
       rel="noopener noreferrer"
-      className={`text-left flex flex-wrap items-stretch col-start-1 col-span-12 even:md:col-start-7 odd:md:col-start-2 md:col-span-5 even:lg:col-start-7 odd:lg:col-start-3 lg:col-span-4 w-full font-akatab mb-4 bg-white hover:bg-[image:var(--bg-linear-gradient-light)] transition-border duration-300 ease-in-out shadow-card hover:shadow-2xl border-2 border-white hover:border-white rounded-lg p-5`}
+      className={`relative bg-white text-left flex flex-wrap items-stretch col-start-1 col-span-12 even:md:col-start-7 odd:md:col-start-2 md:col-span-5 even:lg:col-start-7 odd:lg:col-start-3 lg:col-span-4 w-full font-akatab mb-4 card-bg transition-all duration-300 ease-in-out shadow-card hover:shadow-card-lg border-2 border-white hover:border-white rounded-lg p-5`}
     >
-      <section className="flex flex-wrap w-full">
+      <section className="flex flex-wrap w-full relative">
         <span className="flex justify-between items-start w-full">
-          <h2 className="text-gradient text-h2 leading-h2 font-black mb-2">
+          <h2 className="text-gradient text-h2 leading-h2 font-black mb-4">
             {result?.name}
           </h2>
-          {result?.webUrl && webIcon}
+          <span className="ml-2">{result?.webUrl && webIcon}</span>
         </span>
         {result?.address && (
-          <p className="text-body leading-body">{result?.address}</p>
+          <p className="text-body leading-body mb-2">
+            <span className="absolute w-[18px] h-[18px]">{locationIcon}</span>
+            <span className="inline-block pl-6">{result?.address}</span>
+          </p>
         )}
-        <p>Phone: {formattedPhone}</p>
+        <p className="block w-full text-body leading-body">
+          <span className="absolute w-[18px] h-[18px]">{phoneIcon}</span>
+          <span className="inline-block pl-6">{formattedPhone}</span>
+        </p>
       </section>
       {result.rating && (
         <span className="relative flex justify-end items-end w-full mt-2">
