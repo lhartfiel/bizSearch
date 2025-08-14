3328194735897d481b5432271d2ef4fdef1f3f02
Lindsay Hartfiel <lkhartfiel@uwalumni.com>
2025-08-13 22:09:30 -0600

    Add Vitest and React Testing library
    - Add tests for a simple Footer component
    - Ensure correct test setup and config files to run tests

---
+++ vitest-setup.ts
@@ -0,0 +1,8 @@
+/// <reference types="@vitest/browser/context" />
+import "@testing-library/jest-dom";
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
+
+afterEach(() => {
+  cleanup();
+});
